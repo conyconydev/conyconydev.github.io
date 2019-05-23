9ef02e073f8519ab06d5239da628a63e8d10eb22
conyconydev <conyconydev@gmail.com>
2019-05-23 14:15:16 +0900

    update iosPosts

--- _posts/makePost.sh
+++ _posts/makePost.sh
@@ -19,7 +19,7 @@ tags: [(swift,ios,etc,memo,study), 서브 태그명(부연설명)]
 
 <pre><code class="swift">
 //소스코드 작성.
-// class = "사용 언어"
-// class = "plaintext"
+// class = "사용 언어" -> 큰 따옴표 꼭 다시 확인해서 붙이기 큰따옴표가 없으면 인식안됨
+// class = "plaintext"  -> 그냥 텍스트 상자일 경우 이걸 사용할꺼다.
 </code></pre>
 " > $file_name
